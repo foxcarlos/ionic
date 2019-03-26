0d6919326c1ce36e2ead3645a9b7e3f0b5032c91
foxcarlos <foxcarlos@gmail.com>
2019-03-26 15:19:41 -0300

    [ADD] miscelaneos

---
+++ miscelaneos/src/app/app-routing.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: 'home', loadChildren: './home/home.module#HomePageModule' },
+  { path: 'pagina2', loadChildren: './pagina2/pagina2.module#Pagina2PageModule' },
+  { path: 'pagina3', loadChildren: './pagina3/pagina3.module#Pagina3PageModule' },
+];
+
+@NgModule({
+  imports: [
+    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
+  ],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
